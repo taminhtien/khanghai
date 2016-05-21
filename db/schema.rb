7698676e6a43adef0e95a678caa84e525c08fb52
Tien Ta <taminhtien1993@gmail.com>
2016-05-21 23:45:51 +0700

    Add categories table and Category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521162555) do
+ActiveRecord::Schema.define(version: 20160521164505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
