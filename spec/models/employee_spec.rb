232143067facf9b60abecad44fb5f637e75e83be
Tien Ta <taminhtien1993@gmail.com>
2016-05-21 23:46:20 +0700

    Wrong words

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Employee do
-  context 'associations' do
+  context 'validations' do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :position }
     it { is_expected.to validate_presence_of :description }
--- spec/models/our_service_spec.rb
+++ spec/models/our_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe OurService do
-  context 'associations' do
+  context 'validations' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :content }
   end
