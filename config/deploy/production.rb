90d1a3e8169e7b8f0c847f10f3575ba54e222766
Tien Ta <taminhtien1993@gmail.com>
2016-07-04 23:13:10 +0700

    Update deploy server address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '172.31.27.101', user: 'deploy', roles: %w{web app db}
+server '54.169.102.107', user: 'deploy', roles: %w{web app db}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
