a3f3ff1ff48f488fe6d268e5675b1610a8e817ca
Tien Ta <taminhtien1993@gmail.com>
2016-07-10 15:13:30 +0700

    Add under instruction page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,6 @@
 class HomeController < ApplicationController
+  layout 'coming_soon', only: [:coming_soon]
+
   def index; end
+  def coming_soon; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'home#coming_soon'
+  get '/index', to: 'home#index'
   resource :our_services
 end
