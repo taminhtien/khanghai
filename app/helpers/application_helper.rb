fd072c54c5f250bf15f1dce0485a13c611089ea5
Tom Ta <tom@futureworkz.com>
2017-05-27 18:48:42 +0700

    Update some new stuffs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,21 @@ module ApplicationHelper
   def comma_seperated_attachments_for(list, field = :file)
     list.collect do |attachment|
       link_to attachment.send("#{field}_file_name"), attachment.send(field).url, target: :_blank
-    end.join(", ").html_safe
+    end.join("<div></div>").html_safe
+  end
+
+  def comma_seperated_for(list, field = :title)
+    list.pluck(field).join(", ") unless list.empty?
+  end
+
+  def admin_comma_seperated_links_for(list, field = :title)
+    collection = []
+    list.collect do |item|
+      value = item.send(field)
+      if !value.blank?
+        collection << link_to(value, [:admin, item])
+      end
+    end
+    collection.join(", ").html_safe
   end
 end
